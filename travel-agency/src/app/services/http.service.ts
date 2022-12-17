87d52faba7b78df6a41ecb011cfa86403fda1e88
xramzesx <xramzesbusiness@gmail.com>
2022-12-17 23:32:40 +0100

    [lab5] connect web app with backend

--- travel-agency/src/app/services/http.service.ts
+++ travel-agency/src/app/services/http.service.ts
@@ -1,12 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiPaths } from '@app/api/paths';
 import { TripItem } from '@app/types';
-
-
-const config = {
-  countriesUrl : './assets/countries.json',
-  tripsUrl : './assets/trips.json'
-}
+import { environment } from 'src/environments';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +10,25 @@ const config = {
 export class HttpService {
   trips = []
 
+  preparePath( path : string, id? : string | number ) {
+    return `${
+      environment.apiUrl
+    }${
+      path
+    }${ 
+      id !== null && id !== undefined
+        ? '/' + id 
+        : '' 
+    }`
+  }
+
   constructor(private httpClient: HttpClient) {}
   
   getTrips() {
-    return this.httpClient.get<TripItem[]>(config.tripsUrl)
+    return this.httpClient.get<TripItem[]>(this.preparePath( ApiPaths.trips ))
   }
 
   getCountries() {
-    return this.httpClient.get<string[]>(config.countriesUrl)
+    return this.httpClient.get<string[]>(this.preparePath( ApiPaths.countries ))
   }
 }
