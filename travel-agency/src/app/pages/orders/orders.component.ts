7e1952319d7e3120b8cc0d9377dcf52dfdb9fbd0
xramzesx <xramzesbusiness@gmail.com>
2022-12-19 23:07:37 +0100

    [lab5] add orders

--- travel-agency/src/app/pages/orders/orders.component.ts
+++ travel-agency/src/app/pages/orders/orders.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { GlobalStateService } from '@app/services/global-state.service';
+import { OrderItem, OrderStatus, TripDate } from '@app/types';
+import Utils from '@app/utils';
 
 @Component({
   selector: 'app-orders',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent {
+  constructor (
+    private globalState : GlobalStateService
+  ) {
+    this.statuses = [
+      "All",
+      OrderStatus.Waiting,
+      OrderStatus.Active,
+      OrderStatus.Archive
+    ]
+  }
+
+  statuses : string [] = []
+  status : string = "All"
+
+  onStatusChange (event : any) {
+
+    this.status = event.target.value
+  }
+
+  get orders () : OrderItem[] {
+    return this.globalState.orders.filter( 
+      order => this.status == 'All' || this.getStatus(order) == this.status
+     )
+  }
+
+  getTotal ( order : OrderItem ) {
+    return order.quantity * (order.trip?.price ?? 1) ;
+  }
+  getPrice( order : OrderItem ) {
+    return order.trip?.price ?? 0
+  }
+
+  getDate( order : OrderItem, type : string ){
+    const date : any = order.trip?.date ?? {start : new Date(), end: new Date()}
+
+    return new Date(date[type]) ;
+  }
+
+  getStatus ( order : OrderItem ){
+    const date : any = order.trip?.date ?? {start : new Date(), end: new Date()}
+    
+    return Utils.getStatus(date.start, date.end)
+  }
+
+  getPath ( order : OrderItem ) {
+    return `/offer/${order.tripId}`
+  }
 
 }
